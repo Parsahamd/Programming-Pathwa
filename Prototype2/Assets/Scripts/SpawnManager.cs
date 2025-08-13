bb3e8610030407cd3d0adaa65ea3311602bdac63
Parsahamd <parsa.hamd@gmail.com>
2025-08-13 03:25:33 +0200

    prototype2 spawner

---
+++ Prototype2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
